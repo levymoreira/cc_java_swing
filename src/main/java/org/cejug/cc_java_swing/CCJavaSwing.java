27ebf2fa922f999028f7ff0d356a59a7e10cead9
heliofrota <heliofrota@gmail.com>
2013-05-19 23:10:21 -0300

    modificando para JPA...

--- src/main/java/org/cejug/cc_java_swing/CCJavaSwing.java
+++ src/main/java/org/cejug/cc_java_swing/CCJavaSwing.java
@@ -1,6 +1,14 @@
 package org.cejug.cc_java_swing;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.SwingUtilities;
+
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 /**
  * Classe principal para inciar o sistema.
@@ -17,6 +25,17 @@ public class CCJavaSwing {
      */
     public static void main(String... args) {
 
+        Map<String, String> persistenceUnitProperties = new HashMap<>();
+        
+        persistenceUnitProperties.put(PersistenceUnitProperties.DDL_GENERATION, "create-tables");
+        persistenceUnitProperties.put(PersistenceUnitProperties.DDL_GENERATION_MODE, "database");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("cc_java_swing_pu", persistenceUnitProperties);
+        entityManagerFactory.createEntityManager();
+        entityManagerFactory.createEntityManager(persistenceUnitProperties);
+
+        
+        
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
